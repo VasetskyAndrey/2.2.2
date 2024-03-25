35a03c4950bb2367f46dae7170fb36b0f6b49ecc
Vasetsky Andrey <regboxforu@gmail.com>
2024-03-25 22:44:48 +0300

    начальный коммит

---
+++ src/main/java/web/controller/Car.java
@@ -0,0 +1,14 @@
+package web.controller;
+
+public class Car {
+    private String brand;
+    private String color;
+    private int price;
+
+    public Car(String brand, String color, int price) {
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+    }
+
+}
--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -11,14 +11,32 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/")
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "index";
-	}
-	
+    @GetMapping(value = "/")
+    public String printWelcome(ModelMap model) {
+
+        List<String> messages = new ArrayList<>();
+
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+
+        model.addAttribute("messages", messages);
+
+        return "index";
+    }
+
+    @GetMapping(value = "/cars")
+    public String printCars(ModelMap model) {
+
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car("audi", "black", 52642));
+        cars.add(new Car("mercedes", "red", 57123));
+        cars.add(new Car("bmw", "green", 45678));
+        cars.add(new Car("volvo", "blue", 32456));
+        cars.add(new Car("ford", "white", 32456));
+
+        return "cars";
+    }
+
 }
